cbd7a6ea13bdefa123e78e82dd591756ae98175a
Thierry DECKER <mail@thierry-decker.com>
2019-01-25 09:35:20 +0100

    Add basic parser for agent and server

--- myason.py
+++ myason.py
@@ -2,6 +2,9 @@
 # -*- coding: utf-8 -*-
 
 
+import argparse
+
+
 def main():
     """The entry point of the script
 
@@ -16,7 +19,17 @@ def main():
         None
 
     """
-    return
+
+    # create the top-level parser
+    parser = argparse.ArgumentParser(prog='myason')
+    subparsers = parser.add_subparsers(help='either agent or server instance', dest='app')
+    subparsers.required = True
+    # create the parser for "agent" command
+    parser_agent = subparsers.add_parser(name='agent', help='agent help')
+    # create the parser for "server" command
+    parser_server = subparsers.add_parser(name='server', help='server help')
+    # parse arguments
+    print(parser.parse_args())
 
 
 if __name__ == '__main__':
