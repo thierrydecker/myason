25aba788784c4f52a676af44150517327df61198
Thierry DECKER <mail@thierry-decker.com>
2019-02-04 12:38:42 +0100

    Switch from '' to ""

--- myason.py
+++ myason.py
@@ -10,15 +10,15 @@ import collector
 
 def main():
     # create the top-level parser
-    parser = argparse.ArgumentParser(prog='myason')
-    subparsers = parser.add_subparsers(help='either agent or collector instance', dest='app')
+    parser = argparse.ArgumentParser(prog="myason")
+    subparsers = parser.add_subparsers(help="either agent or collector instance", dest="app")
     subparsers.required = True
     # create the parser for "agent" command
-    parser_agent = subparsers.add_parser(name='agent', help='agent help')
+    parser_agent = subparsers.add_parser(name="agent", help="agent help")
     parser_agent.add_argument("-lc", "--logger-conf")
     parser_agent.add_argument("-ac", "--agent-conf")
     # create the parser for "server" command
-    parser_server = subparsers.add_parser(name='collector', help='server help')
+    parser_server = subparsers.add_parser(name="collector", help="server help")
     # parse arguments
     arguments = parser.parse_args()
     # start the agent or the server
