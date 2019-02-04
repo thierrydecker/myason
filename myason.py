7e6498ee9fe6df8326de6d6431aac5a4254dc81e
Thierry DECKER <mail@thierry-decker.com>
2019-02-04 12:21:18 +0100

    Rename server to collector

--- myason.py
+++ myason.py
@@ -11,19 +11,19 @@ import server
 def main():
     # create the top-level parser
     parser = argparse.ArgumentParser(prog='myason')
-    subparsers = parser.add_subparsers(help='either agent or server instance', dest='app')
+    subparsers = parser.add_subparsers(help='either agent or collector instance', dest='app')
     subparsers.required = True
     # create the parser for "agent" command
     parser_agent = subparsers.add_parser(name='agent', help='agent help')
     # create the parser for "server" command
-    parser_server = subparsers.add_parser(name='server', help='server help')
+    parser_server = subparsers.add_parser(name='collector', help='server help')
     # parse arguments
     arguments = parser.parse_args()
     # start the agent or the server
     if arguments.app == 'agent':
         agent.agent()
     else:
-        server.server()
+        server.collector()
 
 
 if __name__ == '__main__':
