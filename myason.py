f3dc395d6217a06d131312ba6dfb03d4105cf94e
Thierry DECKER <mail@thierry-decker.com>
2019-02-07 07:25:38 +0100

    Add ifconfig application

--- myason.py
+++ myason.py
@@ -6,20 +6,23 @@ import argparse
 
 import agent
 import collector
+import ifconfig
 
 
 def main():
-    # create the top-level parser
+    # Create the top-level parser
     parser = argparse.ArgumentParser(prog="myason")
-    subparsers = parser.add_subparsers(help="either agent or collector instance", dest="app")
+    subparsers = parser.add_subparsers(help="", dest="app")
     subparsers.required = True
-    # create the parser for "agent" command
+    # Create the parser for "agent" command
     parser_agent = subparsers.add_parser(name="agent", help="agent help")
     parser_agent.add_argument("-lc", "--agent-logger-conf", default="agent_logger.yml")
     parser_agent.add_argument("-ac", "--agent-conf", default="agent.yml")
-    # create the parser for "collector" command
-    parser_collector = subparsers.add_parser(name="collector", help="server help")
-    # parse arguments
+    # Create the parser for "collector" command
+    parser_collector = subparsers.add_parser(name="collector", help="collector help")
+    # Create the parser
+    parser_ifconfig = subparsers.add_parser(name="ifconfig", help="Prints list of available adapters")
+    # Parse arguments
     arguments = parser.parse_args()
     if arguments.app == "agent":
         # Start agent
@@ -27,10 +30,12 @@ def main():
             agent_conf_fn=arguments.agent_conf,
             logger_conf_fn=arguments.agent_logger_conf
         )
+    elif arguments.app == "collector":
+        # Start collector
+        collector.collector()
     else:
-        # start collector
-        # collector.collector()
-        pass
+        # Starts ifconfig
+        ifconfig.ifconfig()
 
 
 if __name__ == '__main__':
